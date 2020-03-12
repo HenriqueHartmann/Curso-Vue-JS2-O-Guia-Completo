0f320827bc590ab9474c82004c76cb99e93c2e60
Hartmann <alemaohartmann@gmail.com>
2020-03-12 16:37:01 -0300

    :rocket::sparkles: Exercício da aula 173

---
+++ formularios/formulario-desafio/src/components/NomeCompleto.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="fullname">
+        {{ nome }} {{ sobrenome }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: {
+            type: String,
+            required: true
+        },
+        sobrenome: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
